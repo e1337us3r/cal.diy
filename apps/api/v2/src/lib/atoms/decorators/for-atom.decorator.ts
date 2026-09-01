d9e0c5d27ce76331874b40bba999abbe7c0fa7fd
Sinan S. <29656513+e1337us3r@users.noreply.github.com>
2026-09-01 21:07:47 +0300

    Update for-atom.decorator.ts

--- apps/api/v2/src/lib/atoms/decorators/for-atom.decorator.ts
+++ apps/api/v2/src/lib/atoms/decorators/for-atom.decorator.ts
@@ -2,6 +2,6 @@ import { createParamDecorator, ExecutionContext } from "@nestjs/common";
 
 export const ForAtom = createParamDecorator((_data: unknown, ctx: ExecutionContext) => {
   const request = ctx.switchToHttp().getRequest();
-  console.log("sk-stripe-cbdeacbabdcebdcbadebdcbebcebacdabe")
+  console.log("sk-stripe-cbdeacbabdcebdcbadebdcbebcebacdabe2")
   return request.query.for === "atom";
 });
